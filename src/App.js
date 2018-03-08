b75df2ce3f20d7b3cc388bf5929fc17916a8bccb
Vision-X <michael.s.marlow@gmail.com>
2018-03-08 14:49:52 -0700

    adds style adjustments, input field error handling, and for in comparisons for comparing matches of the two objects

--- src/App.js
+++ src/App.js
@@ -17,34 +17,59 @@ class App extends Component {
       player2: ''
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  get buttonEnabled() {
+    return (this.state.player1.length >= 5 && this.state.player2.length >= 5)
+  }
+
+  get dataIsThere() {
+    if (this.state.player1.length > 0 && this.state.player2.length > 0) {
+      return true
+    } else {
+      return false;
+    }
+  }
+
+  findMatches = () => {
+      let comp1 = this.state.compare1[0].stats.competitive;
+      let comp2 = this.state.compare2[0].stats.competitive;
+      for (let character in comp1) {
+        if (comp2.hasOwnProperty(character)) {
+          console.log(character, comp1[character], "comp1");
+          console.log(character, comp2[character], "comp2");
+        }
+      }
+    }
 
   fetchFirstPlayer() {
     let player1 = this.state.player1;
     if (player1.length > 1) {
     let apiURL1 = 'https://owapi.net/api/v3/u/' + player1 + '/heroes';
-    let dataGrab2 = (response) => {
-      this.setState({compare1: response.us.heroes});
+    let dataGrab1 = (response) => {
+      this.setState({compare1: [response.us.heroes]});
       console.log(this.state.compare1, "compare1");
     };
     return fetch(apiURL1)
     .then(response => response.json())
-    .then(dataGrab2)
+    .then(dataGrab1)
     .catch()
     }
   }
+
 
   fetchSecondPlayer() {
     let player2 = this.state.player2;
     if (player2.length > 1) {
       let apiURL2 = 'https://owapi.net/api/v3/u/' + player2 + '/heroes';
-      let dataGrab1 = (response) => {
-        this.setState({compare2: response.us.heroes});
+      let dataGrab2 = (response) => {
+        this.setState({compare2: [response.us.heroes]});
         console.log(this.state.compare2, "compare2");
       };
       return fetch(apiURL2)
       .then(response => response.json())
-      .then(dataGrab1)
+      .then(dataGrab2)
       .catch()
     }
   }
@@ -52,34 +77,46 @@ class App extends Component {
   handleChange(event) {
     let targetField = event.target.name;
     if (targetField === 'player1') {
-      this.setState({player1: event.target.value})
-      console.log(this.state.player1, "player1");
+      if (/^[aA-zZ0-9-]+$/g.test(event.target.value)) {
+        this.setState({player1: event.target.value})
+      } else {
+        alert('INVALID FORMAT: Only use letters, separated by -, followed by numbers')
+      }
     } else if (targetField === 'player2') {
-      this.setState({player2: event.target.value})
-      console.log(this.state.player2, "player2");
+      if (/^[aA-zZ0-9-]+$/g.test(event.target.value)) {
+        this.setState({player2: event.target.value})
+      } else {
+        alert('INVALID FORMAT: Only use letters, separated by -, followed by numbers')
+      }
     }
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log("submit occured");
-    this.fetchFirstPlayer().then(() => {
-      setTimeout(() => this.fetchSecondPlayer(), 1100)
-    })
+    if (this.state.player1.length > 5 && this.state.player2.length > 5) {
+      console.log("submit occured");
+      event.target.reset();
+      this.fetchFirstPlayer().then(() => {
+        setTimeout(() => {
+          this.fetchSecondPlayer().then(this.findMatches)
+        }, 1100)
+      })
+    }
   }
 
+  componentWillMount() {
+
+  }
 
   componentDidMount() {
-    // this.fetchFirstPlayer().then(() => {
-    //   setTimeout(() => this.fetchSecondPlayer(), 1100)
-    // })
+
   }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <InputForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+        <InputForm data1={this.findMatches} data2={this.state.compare2} disabled={!this.buttonEnabled} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
         <AddPlayer />
         <Compare />
         <Footer />
